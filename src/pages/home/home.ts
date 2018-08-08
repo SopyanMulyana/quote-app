94056cc45c9dd5abc90ac1e2aa55c0f60facbfd5
Sopyan Mulyana <sopyan.mulyana@btpn.com>
2018-08-09 00:53:54 +0700

    customize home view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  colors=["primary","secondary","danger","light","dark"];
+  color="primary";
+  constructor(public navCtrl: NavController, public actionsheetCtrl: ActionSheetController) {
 
+  }
+
+  openMenu() {
+    let actionSheet = this.actionsheetCtrl.create({
+      title: 'About',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Dibuat oleh S.M bekerjasama dengan Kabim Unpad',
+          // role: 'destructive',
+          icon: 'brush',
+          handler: () => {
+            // console.log('Delete clicked');
+          }
+        },
+        {
+          text: 'Developing using IONIC Framework',
+          icon: 'ionic',
+          handler: () => {
+            // console.log('Share clicked');
+          }
+        },
+        {
+          text: 'Back',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: 'close',
+          handler: () => {
+            // console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+  setColor(){
+    this.color = this.colors[2];
   }
 
 }
